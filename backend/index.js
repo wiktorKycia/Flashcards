2740a33c3979c4d416505f6d2c3a9d46bdbe8d94
wiktor <wiktor.kycia@gmail.com>
2026-04-04 18:19:59 +0200

    create the hello world endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.status(200).json({content: "Hello world!"})
+})
+
 app.listen(3000, () => {
     console.log('App is running on http://localhost:3000')
 })
