a1fcc92adcb0db02134285dc426f431c75628a7a
wiktor <wiktor.kycia@gmail.com>
2026-04-06 13:24:12 +0200

    display the data correctly in Home component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,9 +2,11 @@ import {useGetAPI} from "../../hooks/useGetAPI.ts";
 
 export default function Home()
 {
-    const {data} = useGetAPI('/')
+    const {data} = useGetAPI('http://localhost:3000/')
+
+    console.log(data)
 
     return (
-        <div>{data}</div>
+        <div>{data.content}</div>
     )
 }
