bd1b7d9e7086c6f52b65083a64b1cc54be53ac23
wiktor <wiktor.kycia@gmail.com>
2026-04-10 17:21:05 +0200

    create an AttachedFlashcardsMode component in order to have minimized flashcards mode

---
+++ frontend/src/components/AttachedFlashcardsMode/AttachedFlashcardsMode.tsx
@@ -0,0 +1,25 @@
+
+
+export default function AttachedFlashcardsMode() {
+    return (
+        <>
+            <div>
+                <button></button> {/* oddzielny komponent na wielką fiszkę */}
+            </div>
+            <div>
+                <label htmlFor="track_progress">Śledź postępy</label> {/*tylko dla użytkowników zalogowanych*/}
+                <input type="checkbox" name="" id="track_progress"/>
+
+                <button>left icon</button>
+                <div>1 / 30</div>
+                <button>right icon</button>
+
+                <button>back</button> {/*tylko jak checkbox ze śledzeniem postępów jest zaznaczony*/}
+
+                <button>change order</button> {/*takie tasowanie*/}
+                <button>settings</button>
+                <button>full screeen</button>
+            </div>
+        </>
+    )
+}
