5f8a61609b27a53a1efbaca83004642dd42441d3
wiktor <wiktor.kycia@gmail.com>
2026-04-11 22:29:26 +0200

    include the index.scss file, so that the browser can see it

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import App from './App.tsx'
+import './index.scss'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
