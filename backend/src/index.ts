9afe7816a093e3cd0a0bde1adca75769853113d4
JanTopolewski <jankotopolewski@gmail.com>
2026-04-11 23:33:47 +0200

    Add routers to index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
-import express, {Request, Response} from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import dotenvExpand from 'dotenv-expand'
+import usersRouter from "./routers/usersRouter"
+import foldersRouter from "./routers/foldersRouter"
+import flashcardsRouter from "./routers/flashcardsRouter"
+import quizzesRouter from "./routers/quizzesRouter"
+import quizzesProgressRouter from "./routers/quizzesProgressRouter"
+import savedQuizzesRouter from "./routers/savedQuizzesRouter"
 
 const myenv = dotenv.config({ path: '.env.app' })
 dotenvExpand.expand(myenv)
@@ -13,8 +19,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use("/users", usersRouter)
+app.use("/folders", foldersRouter)
+app.use("/saved-quizzes", savedQuizzesRouter)
+app.use("/flashcards", flashcardsRouter)
+app.use("/quizzes", quizzesRouter)
+app.use("/quizzes-progress", quizzesProgressRouter)
+
 app.get('/', (_req: Request, res: Response) => {
     res.status(200).json({content: "Hello world!"})
+})
+
+app.all("*", (_req: Request, res: Response) => {
+    res.sendStatus(404)
 })
 
 app.listen(3000, () => {
