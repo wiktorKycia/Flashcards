d18b45476e900f03dc376e077f603a679a04d82a
wiktor <wiktor.kycia@gmail.com>
2026-04-18 22:07:31 +0200

    wrap all the routes with AuthProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,18 @@ import Home from './pages/Home'
 import UserProfile from './pages/UserProfile'
 import { Routes, Route } from 'react-router'
 import Quiz from "./pages/Quiz";
+import { AuthProvider } from '@/context/AuthContext.tsx'
 
 function App() {
     return (
-        <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/profile/' element={<UserProfile/>}/>
-            <Route path='/quiz/' element={<Quiz quizName={"test"}/>}/>
-        </Routes>
+        <AuthProvider>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/profile/' element={<UserProfile/>}/>
+                <Route path='/quiz/' element={<Quiz quizName={"test"}/>}/>
+            </Routes>
+        </AuthProvider>
+
     )
 }
 
