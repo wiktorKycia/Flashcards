7e0fdba1aba598fe235b2ff840b1420d2c628981
wiktor <wiktor.kycia@gmail.com>
2026-04-18 21:47:32 +0200

    create a useLogin hook

---
+++ frontend/src/hooks/useLogin.ts
@@ -0,0 +1,28 @@
+import { useMutation } from '@tanstack/react-query'
+
+interface LoginVariables {
+    password: string
+    login: string
+}
+
+const login = async ({ login, password }: LoginVariables) => {
+    const response = await fetch('/auth/login', {
+        method: 'POST',
+        body: JSON.stringify({login: login, password: password}),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    if (!response.ok) {
+        throw new Error(`HTTP ${response.status}`)
+    }
+
+    return response.json()
+}
+
+export const useLogin = () => {
+    return useMutation({
+        mutationFn: login
+    })
+}
