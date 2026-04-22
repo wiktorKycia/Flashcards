c2d56cc347699505cb042c1f084a5e06986ab764
wiktor <wiktor.kycia@gmail.com>
2026-04-22 21:28:36 +0200

    add some options to UserSettings

--- frontend/src/pages/UserSettings/UserSettings.tsx
+++ frontend/src/pages/UserSettings/UserSettings.tsx
@@ -1,11 +1,26 @@
 import styles from './UserSettings.module.scss'
-
+import { useAuth } from '@/context/AuthContext.tsx'
+import { useNavigate } from 'react-router-dom'
 
 export default function UserSettings()
 {
+    const navigate = useNavigate()
+    const auth = useAuth()
+
+    const isLoggedIn = !!auth.token
+    if(isLoggedIn)
+    {
+        navigate('/login')
+    }
+
     return (
         <div className={styles.UserSettings}>
-            user settings
+
+            <h1>Hello {auth.user?.name}</h1>
+
+            <button onClick={auth.logout}>Logout</button>
+            <button>Reset password</button>
+            <button>Edit username</button>
         </div>
     )
 }
