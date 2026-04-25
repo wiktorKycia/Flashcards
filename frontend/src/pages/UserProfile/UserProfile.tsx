5a5cb128dec3aa36fb0f64ef840f14636ec305ab
wiktor <wiktor.kycia@gmail.com>
2026-04-25 21:27:58 +0200

    display the info from the hook in UserProfile component

--- frontend/src/pages/UserProfile/UserProfile.tsx
+++ frontend/src/pages/UserProfile/UserProfile.tsx
@@ -1,12 +1,27 @@
-import styles from './UserProfile.module.scss'
+// import styles from './UserProfile.module.scss'
 import { useParams } from 'react-router'
-// import { useAuth } from '@/context/AuthContext.tsx'
+import { useUserInfo } from '@/hooks/useUserInfo.ts'
+import Container from '@/components/Container'
 
 export default function UserProfile() {
-    // const isLoggedIn = !!useAuth().token
     const userId: number = parseInt(useParams().id as string)
 
+    const { data } = useUserInfo(userId)
+
     return (
-        <div className={styles.UserProfile}>information about the user {userId}</div>
+        // <div className={styles.UserProfile}>information about the user {userId}</div>
+        <>
+            {data && (
+                <h1>{data.name}</h1>
+
+            )}
+            {data && data.createdQuizzes.map((quiz) => (
+                <Container>
+                    <h2>{quiz.name}</h2>
+                    <p>{quiz.description}</p>
+                    <p>{quiz.id}</p>
+                </Container>
+            ))}
+        </>
     )
 }
