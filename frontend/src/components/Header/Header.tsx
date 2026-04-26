30c76549f67c3948f1b6a270e471fc16959d3ea6
wiktor <wiktor.kycia@gmail.com>
2026-04-26 16:29:29 +0200

    implement some light-dark mode switch

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,10 +3,12 @@ import Logo from '../Logo/Logo.tsx'
 import SearchBar from '../SearchBar/SearchBar.tsx'
 import ButtonAdd from '../ButtonAdd/ButtonAdd.tsx'
 import ProfilePicture from '../ProfilePicture/ProfilePicture.tsx'
-
+import useTheme from '@/hooks/useTheme.ts'
 import styles from './Header.module.scss'
 
 export default function Header() {
+    const {theme, toggleTheme} = useTheme()
+
     return (
         <header className={styles.Header}>
             <div>
@@ -15,6 +17,9 @@ export default function Header() {
             </div>
             <SearchBar />
             <div>
+                <button onClick={toggleTheme}>
+                    {theme === "light" ? "Dark" : "Light"}
+                </button>
                 <ButtonAdd />
                 <ProfilePicture />
             </div>
