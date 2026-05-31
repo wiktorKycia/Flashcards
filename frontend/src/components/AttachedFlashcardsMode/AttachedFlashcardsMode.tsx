f1a36ac763df08f5f5117ef10b949cbf897a2d12
wiktor <wiktor.kycia@gmail.com>
2026-05-31 14:57:55 +0200

    add responsive styles for flashcard positioning and button; enhance layout for better user experience

--- frontend/src/components/AttachedFlashcardsMode/AttachedFlashcardsMode.tsx
+++ frontend/src/components/AttachedFlashcardsMode/AttachedFlashcardsMode.tsx
@@ -158,9 +158,9 @@ export default function AttachedFlashcardsMode(props: AttachedFlashcardsModeProp
     return (
         <>
             {!finishedTrackingProgress && isTrackingProgress ? (
-                <>
+                <div className={styles.FlashcardPositioner}>
                     {requiresNextTurn ? (
-                        <button onClick={handleNextTurn}>Następna tura</button>
+                        <button onClick={handleNextTurn} className={styles.NextTurnButton}>Następna tura</button>
                     ) : (
                         <>
                             <BigFlashcard
@@ -178,9 +178,9 @@ export default function AttachedFlashcardsMode(props: AttachedFlashcardsModeProp
                             </Container>
                         </>
                     )}
-                </>
+                </div>
             ) : (
-                <>
+                <div className={styles.FlashcardPositioner}>
                     <BigFlashcard
                         front={flashcards[flashcardsIterator].front}
                         back={flashcards[flashcardsIterator].back}
@@ -195,7 +195,7 @@ export default function AttachedFlashcardsMode(props: AttachedFlashcardsModeProp
                         </div>
                         <button onClick={handleIncrement}>→</button>
                     </Container>
-                </>
+                </div>
             )}
 
             <Container cssClassName={'container-positioner ' + styles.OptionsContainer}>
